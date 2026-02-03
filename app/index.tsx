0054cc1acc33119acc8dd0cad57f35748ba887a1
ThePhoenix56 <ds223pj@student.lnu.se>
2026-02-03 22:12:28 +0100

    Add styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <LinearGradient
+      colors={["#387c6d", "#3b5998", "#198815"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Text
+        style={{
+          fontFamily: "System",
+          fontSize: 20,
+          color: "white",
+          marginTop: 50,
+        }}
+      >
+        Aktuella vädervarningar i Sverige:
+      </Text>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "flex-start",
+    flex: 1,
+  },
+});
